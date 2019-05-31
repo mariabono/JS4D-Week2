f71347f95bd4f108a00ad0b33e8fbf1ef7ef2e69
MariaBono <maria.bono@makingwaves.com>
2019-05-31 11:23:57 +0200

    fixed movement on toggle

--- menu.js
+++ menu.js
@@ -1,10 +1,20 @@
 const toggleTag = document.querySelector("a.toggle-nav")
 const mainTag = document.querySelector("main")
+const navTag = document.querySelector("nav")
 
-// When I click the toggle tag, toggle a class of open on the main tag
+// When I click the toggle tag, toggle a class of open on the main tag. If it's open, the toggletag says close and viceversa.
 
 toggleTag.addEventListener("click", function() {
 
     mainTag.classList.toggle("open")
+    navTag.classList.toggle("open")
 
+
+    if (mainTag.classList.contains("open")) {
+        toggleTag.innerHTML = '<img src="assets/close.svg"> Close'
+    } else {
+        toggleTag.innerHTML = '<img src="assets/menu.svg"> Menu'
+    }
+
+    
 })
