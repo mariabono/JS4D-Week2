8c01055cacd51e728f6f56f4404a42a4acb080e5
MariaBono <maria.bono@makingwaves.com>
2019-07-08 13:26:06 +0200

    End of Project 2
    Finished.

--- slides.js
+++ slides.js
@@ -3,7 +3,8 @@
 const slideArea = document.querySelector("div.slides")
 const images = slideArea.querySelectorAll("img")
 
-// 
+
+// keep track of both the current slide and the z-index
 
 let currentSlide = 0
 let z = 1
@@ -22,9 +23,47 @@ slideArea.addEventListener("click", function(){
 
     z = z + 1
 
+
+    // remove animation from the style for EVERY image
+
+    images.forEach(image => {
+        image.style.animation = ""
+    })
+
+
     //pick the right image
+    
     images[currentSlide].style.zIndex = z
     images[currentSlide].style.animation = "fade 0.5s"
 
+})      
+
+
+
+
+
+// Move images randomly on mouse over.
+
+slideArea.addEventListener("mouseover", function() {
+    
+    images.forEach(image => {
+        const x = 25 * (Math.floor(Math.random() * 5)) - 50
+        const y = 25 * (Math.floor(Math.random() * 5)) - 50
+
+        image.style.transform = `translate(${x}px, ${y}px)`
+
+
+    })
+
+})
+
+
+// Puts images back in place
+
+slideArea.addEventListener("mouseout", function() {
+
+    images.forEach(image =>{
+        image.style.transform = ""
+    })
 
 })
